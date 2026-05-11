6e8b6df8539cd15b3fb4a7ab51cf75434ef08776
Stephan Schröter <stephan.schroeter@porsche.digital>
2026-05-11 15:42:44 +0200

    fix: ci build | sas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: process.env.ASTRO_PUBLIC_BASE_PATH || '',
+});
